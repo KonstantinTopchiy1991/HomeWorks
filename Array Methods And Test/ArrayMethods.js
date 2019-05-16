83845a37e6d287c77d72c9cacd24861e6a4ee28a
tiger91 <kmtopchiy@ukr.net>
2019-05-17 00:27:09 +0300

    complited List and changed ArrayMethods

--- Array Methods And Test/ArrayMethods.js
+++ Array Methods And Test/ArrayMethods.js
@@ -1,8 +1,114 @@
 function ArrayList() {
 
-    this.items = [7, 13, 5, 16, 22, 2];
+    // MyList.call(this);
+    //
+     this.items = [7, 13, 5, 16, 22, 2];
+    //
+    // this.pop = function () {
+    //     let lastItem = this.items[this.items.length - 1];
+    //
+    //     let cutItems = [];
+    //
+    //     for (let i = 0; i < this.items.length - 1; i++) {
+    //         cutItems[i] = this.items[i];
+    //     }
+    //     this.items = cutItems;
+    //     return lastItem;
+    // };
+    //
+    // this.push = function (a) {
+    //     this.items[this.items.length] = a;
+    //     return this.items;
+    // };
+    //
+    // this.shift = function () {
+    //     let firstItem = this.items[0];
+    //     let cutItem = [];
+    //     for (let i = 1; i < this.items.length - 1; i++) {
+    //         cutItem[i - 1] = this.items[i];
+    //     }
+    //     this.items = cutItem;
+    //     return firstItem;
+    // };
+    //
+    // this.unshift = function (a) {
+    //     for (let i = this.items.length; i > 0; i--) {
+    //         this.items[i] = this.items[i - 1];
+    //     }
+    //     this.items[0] = a;
+    //     return this.items;
+    // };
+    //
+    // this.sort = function () {
+    //     for (let i = 0; i < this.items.length; i++) {
+    //         for (let j = 0; j < this.items.length - 1; j++) {
+    //             if (this.items[j] > this.items[j + 1]) {
+    //                 let temp = this.items[j];
+    //                 this.items[j] = this.items[j + 1];
+    //                 this.items[j + 1] = temp;
+    //             }
+    //         }
+    //     }
+    //     return (this.items);
+    // };
+    //
+    // this.splice = function (index, value, ...valuesForInserting) {
+    //     let arrayBeforeRemoving = [];
+    //     for (let i = 0; i < index; i++) {
+    //         arrayBeforeRemoving[i] = this.items[i];
+    //     }
+    //     let arrayAfterRemoving = [];
+    //     let k = 0;
+    //     if (this.items[index + value]) {
+    //         for (let i = index + value; i < this.items.length; i++) {
+    //             arrayAfterRemoving[k] = this.items[i];
+    //             k++;
+    //         }
+    //     }
+    //     this.items = [...arrayBeforeRemoving, ...valuesForInserting, ...arrayAfterRemoving];
+    //
+    //     return this.items;
+    // };
+    //
+    // this.remove = function (index) {
+    //     for(let i = index; i < this.items.length - 1; i++){
+    //         this.items[i]=this.items[i+1];
+    //     }
+    //     delete this.items[this.items.length - 1];
+    //     return this.items;
+    // };
+    //
+    // this.size = function () {
+    //     let size = 0;
+    //     this.items.forEach(function () {
+    //         size++;
+    //     })
+    //     return size;
+    // };
+    //
+    // this.doString = function () {
+    //     let value = '';
+    //     for(let i = 0; i < this.items.length; i++){
+    //         value = value + String(this.items[i]);
+    //     }
+    //     return value;
+    // };
+    //
+    // this.newMap = function (a) {
+    //     let newArray = [];
+    //
+    //     for (let i = 0; i < this.items.length; i++){
+    //         newArray[i] = this.items[i]*a;
+    //     }
+    //     this.items = newArray;
+    //     return this.items;
+    // }
+}
+    MyList.call(this);
+    ArrayList.prototype = Object.create(MyList.prototype);
+    ArrayList.prototype.constructor = ArrayList;
 
-    this.pop = function () {
+    ArrayList.prototype.pop = function () {
         let lastItem = this.items[this.items.length - 1];
 
         let cutItems = [];
@@ -14,12 +120,12 @@ function ArrayList() {
         return lastItem;
     };
 
-    this.push = function (a) {
+    ArrayList.prototype.push = function (a) {
         this.items[this.items.length] = a;
         return this.items;
     };
 
-    this.shift = function () {
+    ArrayList.prototype.shift = function () {
         let firstItem = this.items[0];
         let cutItem = [];
         for (let i = 1; i < this.items.length - 1; i++) {
@@ -29,7 +135,7 @@ function ArrayList() {
         return firstItem;
     };
 
-    this.unshift = function (a) {
+    ArrayList.prototype.unshift = function (a) {
         for (let i = this.items.length; i > 0; i--) {
             this.items[i] = this.items[i - 1];
         }
@@ -37,7 +143,7 @@ function ArrayList() {
         return this.items;
     };
 
-    this.sort = function () {
+    ArrayList.prototype.sort = function () {
         for (let i = 0; i < this.items.length; i++) {
             for (let j = 0; j < this.items.length - 1; j++) {
                 if (this.items[j] > this.items[j + 1]) {
@@ -50,7 +156,7 @@ function ArrayList() {
         return (this.items);
     };
 
-    this.splice = function (index, value, ...valuesForInserting) {
+    ArrayList.prototype.splice = function (index, value, ...valuesForInserting) {
         let arrayBeforeRemoving = [];
         for (let i = 0; i < index; i++) {
             arrayBeforeRemoving[i] = this.items[i];
@@ -68,7 +174,7 @@ function ArrayList() {
         return this.items;
     };
 
-    this.remove = function (index) {
+    ArrayList.prototype.remove = function (index) {
         for(let i = index; i < this.items.length - 1; i++){
             this.items[i]=this.items[i+1];
         }
@@ -76,7 +182,7 @@ function ArrayList() {
         return this.items;
     };
 
-    this.size = function () {
+    ArrayList.prototype.size = function () {
         let size = 0;
         this.items.forEach(function () {
             size++;
@@ -84,7 +190,7 @@ function ArrayList() {
         return size;
     };
 
-    this.doString = function () {
+    ArrayList.prototype.doString = function () {
         let value = '';
         for(let i = 0; i < this.items.length; i++){
             value = value + String(this.items[i]);
@@ -92,7 +198,7 @@ function ArrayList() {
         return value;
     };
 
-    this.newMap = function (a) {
+    ArrayList.prototype.newMap = function (a) {
         let newArray = [];
 
         for (let i = 0; i < this.items.length; i++){
@@ -101,7 +207,7 @@ function ArrayList() {
         this.items = newArray;
         return this.items;
     }
-};
+
 
 
 
---
+++ Array Methods And Test/List.js
@@ -0,0 +1,47 @@
+function MyList() {
+
+    // this.name = "MyList";
+    //
+    // this.pop = function () {};
+    //
+    // this.push = function (a) {};
+    //
+    // this.shift = function () {};
+    //
+    // this.unshift = function (a) {};
+    //
+    // this.sort = function () {};
+    //
+    // this.splice = function (index, value, ...valuesForInserting) {};
+    //
+    // this.remove = function (index) {};
+    //
+    // this.size = function () {};
+    //
+    // this.doString = function () {};
+    //
+    // this.newMap = function (a) {};
+
+    MyList.prototype.pop = function () {};
+
+    MyList.prototype.push = function () {};
+
+    MyList.prototype.shift = function () {};
+
+    MyList.prototype.unshift = function () {};
+
+    MyList.prototype.sort = function () {};
+
+    MyList.prototype.splice = function () {};
+
+    MyList.prototype.remove = function () {};
+
+    MyList.prototype.size = function () {};
+
+    MyList.prototype.doString = function () {};
+
+    MyList.prototype.newMap = function () {};
+
+
+
+}
